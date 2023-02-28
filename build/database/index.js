d55fa0fbc85d45188b78d6f1aa227015cbbd5f91
victorbraga <victor.medeiros@contraprova.com.br>
2023-02-28 18:46:17 -0300

    Correção Vercel

--- build/database/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var typeorm_1 = require("typeorm");
-typeorm_1.createConnection();
-if (!typeorm_1.createConnection()) {
-    console.log("Conexao com problemas.");
-}
-//# sourceMappingURL=index.js.map
